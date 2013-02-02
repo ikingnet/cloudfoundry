0a9e3d9cac13dc59060f1feb3af7c8c1c14018b3
ikingnet <zhangzy@xy.com>
2013-02-02 02:17:58 -0800

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p ~/cloudfoundry/vcap/uaa/uaa/target
+wget https://repo.springsource.org/snapshot/org/cloudfoundry/runtime/cloudfoundry-identity-uaa/1.0.0.BUILD-SNAPSHOT/cloudfoundry-identity-uaa-1.0.0.BUILD-20120329.165548-2.war -P ~/cloudfoundry/vcap/uaa/uaa/target/
